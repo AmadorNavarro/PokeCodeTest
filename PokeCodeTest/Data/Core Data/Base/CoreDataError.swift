9076e5945d15a5284963b1a6d9b45cedf134547d
Amador Navarro <anavarro@opentrends.net>
2019-07-28 13:22:56 +0200

    Added Core Data stack and xcdatamodel

---
+++ PokeCodeTest/Data/Core Data/Base/CoreDataError.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataError.swift
+//  PokeCodeTest
+//
+//  Created by Amador Navarro Lucas on 11/09/2018.
+//  Copyright © 2018 AmadorNavarro. All rights reserved.
+//
+
+import Foundation
+
+public enum CoreDataError: Error {
+    
+    case entityDuplicate, none
+    
+}
